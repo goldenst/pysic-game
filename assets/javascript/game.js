dd54c149d954bf090dd9259c876aa85beabaa5fd
MSI\jeff <hatcher28@me.com>
2018-12-24 21:32:58 -0800

    finished logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,41 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-
+var gussedLetters = [];
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var computerGuess = "";
 // get computer guess
-var computerGuess = options[Math.floor(Math.random() * options.length)];
+function getLetter () {
+   computerGuess = options[Math.floor(Math.random() * options.length)];
+}
+getLetter()
 
-console.log("Comp guess " + computerGuess);
+// empty arary function
+function empty() {
+  gussedLetters = [];
+}
+
 // get users guess
 document.onkeyup = function () {
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
   // decrese guesses left
   guessesLeft--
-  console.log("User Guess: " + userGuess);
-
-
-
+  // push guesses to array
+  gussedLetters.push(userGuess)
+  // if guess matches
   if (computerGuess === userGuess) {
     wins++
     guessesLeft = 9
+    empty()
+    getLetter()
   }
-
+  // if guesses do not match 
   if (computerGuess != userGuess && guessesLeft === 0) {
     // Increase looses
     losses++
     guessesLeft = 9
+    empty()
+    getLetter()
   }
 
   // update guesses left count
@@ -34,14 +44,17 @@ document.onkeyup = function () {
   // display losses on screen
   // display guess left on screen
   // display gussed lettersd on screen
-  var html = "<p>press any key to continue</p>" +
+  var html = 
     "<p>Wins: " + wins + "</p>" +
     "<p>Loses: " + losses + "</p>" +
     "<p>Guesses Left: " + guessesLeft + "</p>" +
-    "<p>Letters Gussed: " + userGuess + "</p>";
+    "<p>Letters Gussed: " + gussedLetters + "</p>";
 
   document.querySelector("#game").innerHTML = html;
 
+  // testing logs
+  console.log("User Guess: " + userGuess);
+  console.log("Comp guess " + computerGuess);
   console.log("Guesses left:  " + guessesLeft);
 }
 
